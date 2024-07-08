7bd8fb7e9d701f55ae9965ad0d037c3e99f643e8
Tandy Sabana <tansabana12@gmail.com>
2024-07-08 11:39:43 +0700

    Initial Commit
    Initial Commit

---
+++ BasicCalculator/BasicCalculator/BasicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorApp.swift
+//  BasicCalculator
+//
+//  Created by tandyys on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
